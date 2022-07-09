d562fa9475d73023f2a15aeac3b23dbba603e978
luizfetrindade <luiz.fetrindade@gmail.com>
2022-07-08 22:20:11 -0300

    Update git ignore and adds xcode project

---
+++ Energos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Energos
+//
+//  Created by Luiz Felipe Trindade on 08/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
